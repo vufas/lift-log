4fe4445995e3dec8dfa8dbe50c7a817dda19701c
EB <eb@EBs-Mac-mini.local>
2026-05-26 20:00:07 -0700

    Refresh cached app assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lift-log-v7";
+const CACHE_NAME = "lift-log-v8";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -26,5 +26,13 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
